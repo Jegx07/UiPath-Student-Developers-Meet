6e347352fdc9d1105cf26fe77057f57bb7a7b731
Jegatheesh V <jegatheeshv24cb@psnacet.edu.in>
2026-02-10 22:22:54 +0530

    Update RobotSpline.tsx

--- src/components/RobotSpline.tsx
+++ src/components/RobotSpline.tsx
@@ -2,10 +2,10 @@ import { SplineScene } from "@/components/ui/splite";
 
 export const RobotSpline = () => {
   return (
-    <div className="w-full h-[480px] md:h-[600px] lg:h-[680px] relative overflow-visible">
+    <div className="w-[calc(100%+80px)] md:w-[calc(100%+120px)] lg:w-[calc(100%+160px)] -mx-[40px] md:-mx-[60px] lg:-mx-[80px] h-[620px] md:h-[760px] lg:h-[840px] relative overflow-visible flex items-end justify-center">
       <SplineScene
         scene="https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode"
-        className="w-full h-full scale-[1.05] md:scale-[1.1] lg:scale-[1.15]"
+        className="w-full h-full scale-[1]"
       />
     </div>
   );
